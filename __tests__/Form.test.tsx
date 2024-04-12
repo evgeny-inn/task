774cca19375d14e3ff8f3a8b3fc7f25d537b49f7
Evgeny <evgeny.chugaev@innowise.com>
2024-04-12 17:15:22 +0200

    fix unit tests

--- __tests__/Form.test.tsx
+++ __tests__/Form.test.tsx
@@ -3,11 +3,11 @@ import { Form } from '../src/components';
 import renderer from "react-test-renderer";
 
 it("renders update form correctly", () => {
-  const tree = renderer.create(<Form type="update" contactId={1} />).toJSON();
+  const tree = renderer.create(<Form type="update" contactId={1} closeModal={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
 
 it("renders add form correctly", () => {
-  const tree = renderer.create(<Form type="add" contactId={1} />).toJSON();
+  const tree = renderer.create(<Form type="add" contactId={1} closeModal={() => {}} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
