768be06510ee482d91451efa5ba110430ed63fb0
Evgeny <evgeny.chugaev@innowise.com>
2024-04-23 08:55:05 +0200

    add e2e test

---
+++ __tests__/App.test.tsx
@@ -0,0 +1,129 @@
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import '@testing-library/jest-dom';
+import { Polly } from '@pollyjs/core';
+import NodeHttpAdapter from '@pollyjs/adapter-node-http';
+
+import App from '../src/App';
+
+Polly.register(NodeHttpAdapter);
+
+let polly: Polly;
+
+describe('app', () => {
+  beforeAll(() => {
+    polly = new Polly('rest api', {
+      adapters: ['node-http'],
+    });
+
+    const { server } = polly;
+
+    server
+      .get(`/contacts`)
+      .intercept((_, res) => {
+        const data = {
+          responseObject: [
+            {
+              id: 1,
+              firstname: 'Matthias',
+              lastname: 'Käpernick',
+              email: 'dev-jobs@autohaus-koenig.de',
+            },
+          ],
+        };
+        res.status(200).json(data);
+      });
+
+    server
+      .post(`/contacts`)
+      .intercept((_, res) => {
+        const newContact = {
+          firstname: 'Billy',
+          lastname: 'Bauer',
+          email: 'billy.bauer@gmail.com',
+        };
+        res.status(201).json({
+          responseObject: { id: 2, ...newContact },
+        });
+      });
+
+    server
+      .put(`/contacts/2`)
+      .intercept((_, res) => {
+        const updatedContact = {
+          firstname: 'Billy',
+          lastname: 'Johnson',
+          email: 'billy.bauer@gmail.com',
+        };
+        res.status(200).json({
+          responseObject: { id: 2, ...updatedContact }
+        })
+      });
+
+    server
+      .delete(`/contacts/2`)
+      .intercept((_, res) => {
+        res.status(200);
+      })
+  });
+
+  beforeAll(async () => {
+    await render(<App />);
+  });
+
+  afterAll(async () => {
+    await polly.stop();
+  });
+
+  it('peforms CRUD operations', async () => {
+    await waitFor(() => {
+      const contact = screen.getByText('Matthias Käpernick');
+      expect(contact).toBeInTheDocument();
+    });
+
+    const addButton = screen.getByText('Neuer Eintrag');
+    await userEvent.click(addButton);
+
+    const name = screen.getByLabelText(/vorname/i);
+    await userEvent.type(name, 'Billy');
+
+    const surname = screen.getByLabelText(/nachname/i);
+    await userEvent.type(surname, 'Bauer');
+
+    const email = screen.getByLabelText(/e-mail/i);
+    await userEvent.type(email,'billy.bauer@gmail.com');
+
+    const submitButton = screen.getByText('Speichern');
+    await userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Billy Bauer')).toBeInTheDocument();
+    });
+
+    const contact = screen.getByText('Billy Bauer');
+    await userEvent.click(contact);
+
+    const surnameToUpdate = screen.getByLabelText(/nachname/i);
+    await userEvent.clear(surnameToUpdate);
+    await userEvent.type(surnameToUpdate, 'Johnson');
+
+    const saveButton = screen.getByText('Speichern');
+    await userEvent.click(saveButton);
+
+    await waitFor(() => {
+      expect(screen.getByText('Billy Johnson')).toBeInTheDocument();
+      expect(screen.queryByText('Billy Bauer')).not.toBeInTheDocument();
+    });
+
+    const updatedContact = screen.getByText('Billy Johnson');
+    await userEvent.click(updatedContact);
+
+    const deleteButton = screen.getByText('Löschen');
+    await userEvent.click(deleteButton);
+
+    await waitFor(() => {
+      expect(screen.queryByText('Billy Johnson')).not.toBeInTheDocument();
+    });
+  });
+});
--- jest.config.cjs
+++ jest.config.cjs
@@ -3,9 +3,9 @@
 module.exports = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["./setupTests.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  modulePaths: ["<rootDir>"],
+  testEnvironmentOptions: {
+    url: "http://localhost:8080",
+  },
   moduleNameMapper: {
     "\\.(css)$": "<rootDir>/node_modules/identity-obj-proxy",
   },
