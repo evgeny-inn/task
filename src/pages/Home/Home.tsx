e8cd62855f83bad06aa003bf45a790224d39dde7
Evgeny <evgeny.chugaev@innowise.com>
2024-04-23 16:05:49 +0200

    fix contacts grid

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -34,7 +34,7 @@ const Home = () => {
       </div>
       <ul className={styles.contacts}>
         {contacts.map(({ id, firstname, lastname, email }) => (
-          <li key={id} onClick={openUpdateModal(id)}>
+          <li key={id} className={styles.contact} onClick={openUpdateModal(id)}>
             <Contact title={`${firstname} ${lastname}`} text={email} />
           </li>
         ))}
